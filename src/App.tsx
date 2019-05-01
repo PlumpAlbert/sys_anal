3e65e9fb0e54cdd4ae2b30fadd590b4d21da6a4a
plump_albert <plumpalbert@gmail.com>
2019-05-01 18:58:59 +0300

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Graph from "./Graph/graph";
+import Matrix from "./Matrix";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
         </header>
-        <Graph />
+        <div className='app-wrapper'>
+          <Graph />
+          <div className='wrapper'>
+            <Matrix type='Incidence'/>
+            <Matrix type='Adjacency'/>
+          </div>
+        </div>
       </div>
     );
   }
