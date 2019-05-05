41ce2d5339574e260c7c569d2ab2ae019f070410
plump_albert <plumpalbert@gmail.com>
2019-05-05 19:18:22 +0300

    Updated test for Matrix

--- src/tests/Matrix.test.tsx
+++ src/tests/Matrix.test.tsx
@@ -1,11 +1,13 @@
-import { shallow } from "enzyme";
-import { Matrix } from "../components/Matrix";
+import { shallow, mount, render } from "enzyme";
+import { Matrix, TProps } from "../components/Matrix";
 import { GraphNode, Link } from "../store";
 import * as React from "react";
 
 describe("Testing Matrix component", () => {
   let nodes: GraphNode[] = [];
   let links: Link[] = [];
+  let appendNodes: jest.Mock<any, any>;
+  let appendLinks: jest.Mock<any, any>;
   beforeEach(() => {
     nodes = [
       { id: 1, label: "A" },
@@ -17,15 +19,61 @@ describe("Testing Matrix component", () => {
       { source: nodes[2], target: nodes[1], twoWay: true, label: "e1" },
       { source: nodes[2], target: nodes[2], twoWay: true, label: "e2" }
     ];
+    appendNodes = jest.fn();
+    appendLinks = jest.fn();
   });
-  it("Creates incidence matrix correctly", () => {
-    expect(
-      shallow(<Matrix type="Incidence" nodes={nodes} links={links} />)
-    ).toMatchSnapshot();
+
+  describe("Incidence matrix", () => {
+    let props: TProps;
+    beforeEach(() => {
+      props = { type: "Incidence", nodes, links, appendNodes, appendLinks };
+    });
+
+    it("Creates matrix correctly", () => {
+      expect(shallow(<Matrix {...props} />)).toMatchSnapshot();
+    });
+
+    it("Should append column", () => {
+      let matrix = shallow(<Matrix {...props} />);
+      expect(matrix.find("input").length).toBe(2);
+      let rowInput = matrix.find("input").at(0);
+      rowInput.simulate("blur", { target: { value: "new row" } });
+      expect(appendNodes).toBeCalled();
+    });
+
+    it("Should append row", () => {
+      let matrix = shallow(<Matrix {...props} />);
+      expect(matrix.find("input").length).toBe(2);
+      let rowInput = matrix.find("input").at(1);
+      rowInput.simulate("blur", { target: { value: "new row" } });
+      expect(appendNodes).toBeCalled();
+    });
   });
-  it("Creates adjacency matrix correctly", () => {
-    expect(
-      shallow(<Matrix type="Adjacency" nodes={nodes} links={links} />)
-    ).toMatchSnapshot();
+
+  describe("Adjacency matrix", () => {
+    let props: TProps;
+    beforeEach(() => {
+      props = { type: "Adjacency", nodes, links, appendLinks, appendNodes };
+    });
+
+    it("Creates matrix correctly", () => {
+      expect(shallow(<Matrix {...props} />)).toMatchSnapshot();
+    });
+
+    it("Should append column", () => {
+      let matrix = shallow(<Matrix {...props} />);
+      expect(matrix.find("input").length).toBe(2);
+      let rowInput = matrix.find("input").at(0);
+      rowInput.simulate("blur");
+      expect(appendLinks).toBeCalled();
+    });
+
+    it("Should append row", () => {
+      let matrix = shallow(<Matrix {...props} />);
+      expect(matrix.find("input").length).toBe(2);
+      let rowInput = matrix.find("input").at(1);
+      rowInput.simulate("blur", { target: { value: "new row" } });
+      expect(appendNodes).toBeCalled();
+    });
   });
 });
