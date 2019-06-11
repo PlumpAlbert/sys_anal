09a7808ff8d20561d7e23389ad0eb9a143fa1e13
plump_albert <plumpalbert@gmail.com>
2019-06-11 22:26:03 +0300

    Removed twoWay links in initial store

--- src/store/index.ts
+++ src/store/index.ts
@@ -65,12 +65,12 @@ const initNodes: GraphNode[] = [
 ];
 const initLinks: Link[] = [
   { source: initNodes[0], target: initNodes[1], twoWay: false, label: "e0" },
-  { source: initNodes[2], target: initNodes[1], twoWay: true, label: "e1" },
+  { source: initNodes[2], target: initNodes[1], twoWay: false, label: "e1" },
   { source: initNodes[4], target: initNodes[0], twoWay: false, label: "e2" },
   { source: initNodes[2], target: initNodes[5], twoWay: false, label: "e3" },
-  { source: initNodes[4], target: initNodes[6], twoWay: true, label: "e4" },
+  { source: initNodes[4], target: initNodes[6], twoWay: false, label: "e4" },
   { source: initNodes[3], target: initNodes[7], twoWay: false, label: "e5" },
-  { source: initNodes[7], target: initNodes[5], twoWay: true, label: "e6" }
+  { source: initNodes[7], target: initNodes[5], twoWay: false, label: "e6" }
 ];
 
 export const mainReducer: Reducer<IGlobalState, TActions> = (
